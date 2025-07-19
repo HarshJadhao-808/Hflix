dd4524329b1da7df784f41baad093748f4c269c5
Harshjadhao-808 <jadhaoharsh141@gmail.com>
2025-07-19 17:27:33 +0530

    saturday comple ted

--- Mainpage/index.js
+++ Mainpage/index.js
@@ -1,5 +1,7 @@
 let user_info = document.getElementById("user_info");
+let coinbuy = document.getElementById("buycoin");
 let search = document.getElementById("search");
+let login_amount="500";
 let nav_2 = document.getElementById("nav_2");
 const default_nav = () => {
     nav_2.style.width="56%"
@@ -15,6 +17,43 @@ const default_nav = () => {
 };
 
 // default_nav()
+
+const buycoin = () => {
+    down_arrow.removeEventListener("mouseenter", buycoin);
+    coinbuy.style.border = "3px solid rgba(245, 198, 28, 1)";
+    let div=document.createElement("div")
+    div.innerHTML = `
+    <div id=first> 
+    <img id="coinbuy_coin" src="../Images/H logo.svg">
+    <h3>${login_amount}</h3>
+    <h3>balance</h3>
+    <img id="closecoin" src="../Images/arrow-square-left.svg">
+    </div>
+    <div id="second">
+    
+    <div id="buying_div">
+    <img id="coinbuy_second_coin" src="../Images/H logo.svg">
+    <h2>
+    <input id="enteramount" placeholder="1000...">
+    </h2>  
+    <button>
+    <img id="topup" src="../Images/Topup.svg">
+    </button>
+    </div>
+    <div id="buyingoptions">
+    <h2 class="bestbuy">100</h2>
+    <h2 class="bestbuy">200</h2>
+    <h2 class="bestbuy">500</h2>
+    <h2 class="bestbuy">1000</h2>
+
+    </div>
+
+    </div>
+    `;
+  
+    
+    coinbuy.append(div)
+}
 
 const signined_nav = () => {
     user_info.style.display="flex"
@@ -33,11 +72,12 @@ const signined_nav = () => {
     let H_coin = document.createElement("img")
     H_coin.src="../Images/H logo.svg";
     let amount=document.createElement("h3")
-    amount.innerText="500"
+    amount.innerText=login_amount
     let down_arrow=document.createElement("img")
     down_arrow.src = "../Images/arrow-down.svg";
     down_arrow.setAttribute("id","down_arrow")
     coin.append(H_coin,amount,down_arrow)
+    down_arrow.addEventListener("mouseenter",buycoin)
     
     // user div
     
@@ -64,6 +104,15 @@ const signined_nav = () => {
     
     coin.setAttribute("id","coin")
     user_info.append(coin,user)
+    
 };
 
 signined_nav();
+
+// buycoin()
+
+
+const close_coindiv=() =>{
+ console.log("hello");
+    
+}
